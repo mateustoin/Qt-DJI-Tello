8be716b51cb6a494b47caf3cea7ed9711417f3e5
Mateus Antonio <mateustoin@gmail.com>
2021-07-11 22:39:00 -0300

    test: Adding missing parameters to tello response types

--- DJI-Tello/TelloEnumTypes.h
+++ DJI-Tello/TelloEnumTypes.h
@@ -7,13 +7,14 @@ enum class TelloResponse {
     VALUE,              // Answear positive with value return
     ERROR_NOT_JOYSTICK, // Wait untill send next command
     ERROR_NO_VALID_IMU, // Maybe had some changes on room light
+    OUT_OF_RANGE,       // If the range is like 20-500 and you give a number out of the range
+    UNDEFINED           // Used while I don't discover all of error types
 };
 
 enum class TelloAlerts {
     TELLO_CONNECTION_ESTABLISHED,
     TELLO_CONNECTION_FAILED,
     SOCKET_CONNECTION_FAILED,
-
 };
 
 #endif // TELLOENUMTYPES_H
